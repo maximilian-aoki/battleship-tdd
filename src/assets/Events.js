381bf6dfca6e87d3940ef0ad028436430d1c4033
Maximilian Aoki <135271252+maximilian-aoki@users.noreply.github.com>
2024-01-12 15:58:47 -0500

    Write test suite for Events Class

--- src/assets/Events.js
+++ src/assets/Events.js
@@ -1,30 +1,28 @@
-const events = {};
+export default class Events {
+  constructor() {
+    this.events = {};
+  }
 
-// create a new function for a new or existing event name (doesn’t check duplicates)
-function on(eventName, fn) {
-  events[eventName] = events[eventName] || [];
-  events[eventName].push(fn);
-}
+  on(eventName, fn) {
+    this.events[eventName] = this.events[eventName] || [];
+    this.events[eventName].push(fn);
+  }
 
-// remove an event name’s associated function (one instance in this e.g.)
-function off(eventName, fn) {
-  if (events[eventName]) {
-    for (let i = 0; i < events[eventName].length; i += 1) {
-      if (events[eventName][i] === fn) {
-        events[eventName].splice(i, 1);
-        break;
+  off(eventName, fn) {
+    if (this.events[eventName]) {
+      for (let i = 0; i < this.events[eventName].length; i += 1) {
+        if (this.events[eventName][i] === fn) {
+          this.events[eventName].splice(i, 1);
+        }
       }
     }
   }
-}
 
-// emit a piece of data to all functions on one event name
-function emit(eventName, data) {
-  if (events[eventName]) {
-    events[eventName].forEach((fn) => {
-      fn(data);
-    });
+  emit(eventName, data) {
+    if (this.events[eventName]) {
+      this.events[eventName].forEach((fn) => {
+        fn(data);
+      });
+    }
   }
 }
-
-export { on, off, emit };
---
+++ src/assets/Events.test.js
@@ -0,0 +1,31 @@
+import Events from './Events';
+
+describe('Events', () => {
+  const eventHandler = new Events();
+  const mockFunc1 = jest.fn((x) => x + 1);
+  const mockFunc2 = jest.fn((x) => x + 1);
+
+  test('correctly instantiates with new object', () => {
+    expect(eventHandler.events).toEqual({});
+  });
+
+  test('adds new event listener if none present', () => {
+    expect(eventHandler.events).toEqual({});
+    eventHandler.on('newEvent', mockFunc1);
+    expect(eventHandler.events).toEqual({ newEvent: [mockFunc1] });
+  });
+
+  test('delete event listener if present', () => {
+    eventHandler.off('newEvent', mockFunc1);
+    expect(eventHandler.events).toEqual({ newEvent: [] });
+  });
+
+  test('emit data from each subscribed function', () => {
+    eventHandler.on('newEvent', mockFunc1);
+    eventHandler.on('newEvent', mockFunc2);
+    eventHandler.emit('newEvent', 1);
+
+    expect(mockFunc1).toHaveBeenCalledWith(1);
+    expect(mockFunc2).toHaveBeenCalledWith(1);
+  });
+});
